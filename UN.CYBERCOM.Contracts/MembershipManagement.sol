d5972459042a21ba292cf2036c1df1e2ac545880
jlind0 <lind@yahoo.com>
2024-02-19 03:03:22 -0600

    Revert "Update MembershipManagement.sol 🌐 Membership Management Library"
    This reverts commit ef035209e20f31b5d7f85cc668ab03f73c61c466.

--- UN.CYBERCOM.Contracts/MembershipManagement.sol
+++ UN.CYBERCOM.Contracts/MembershipManagement.sol
@@ -1,12 +1,9 @@
-/// SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.7;
 
 import "@openzeppelin/contracts/utils/Strings.sol";
 
 library MembershipManagement {
-    /**
-     * @dev Represents a request to join a council or nation.
-     */
     struct MembershipProposalRequest {
         address member;
         Nation newNation;
@@ -14,47 +11,28 @@ library MembershipManagement {
         uint duration;
     }
 
-    /**
-     * @dev Represents a nation with an identifier and a name.
-     */
     struct Nation {
         address id;
         string name;
     }
-
-    /**
-     * @dev Represents a vote cast by a member.
-     */
-    struct Vote {
+    struct Vote{
         address member;
         bool voteCasted;
         uint timestamp;
         uint proposalId;
     }
-
-    /**
-     * @dev Represents a council with its name, role, voting parameters, and member groups.
-     */
-    struct Council {
+    struct Council{
         string name;
         bytes32 role;
         VotingParameters votingParameters;
         CouncilGroup[] groups;
     }
-
-    /**
-     * @dev Represents a group within a council, consisting of multiple nations.
-     */
-    struct CouncilGroup {
+    struct CouncilGroup{
         uint id;
         string name;
         Nation[] members;
     }
-
-    /**
-     * @dev Parameters defining how voting is conducted within a council.
-     */
-    struct VotingParameters {
+    struct VotingParameters{
         bool randomizeByGroup;
         bool randomizeByMember;
         uint32 outputCountForGroup;
@@ -65,11 +43,7 @@ library MembershipManagement {
         uint sumNumerator;
         bool avgVotes;
     }
-
-    /**
-     * @dev Response to a membership proposal, including the result and votes.
-     */
-    struct MembershipProposalResponse {
+    struct MembershipProposalResponse{
         uint id;
         address member;
         Nation newNation;
@@ -83,50 +57,30 @@ library MembershipManagement {
         address owner;
         address proposalAddress;
     }
-
-    /**
-     * @dev Represents a document with its metadata.
-     */
-    struct Doc {
-        string title;
-        string url;
-        bytes32 dochash;
-        bytes signature;
-        address signer;
+    struct Doc{
+        string  title;
+        string  url;
+        bytes32  dochash;
+        bytes  signature;
+        address  signer;
         address docAddress;
     }
-
-    /**
-     * @dev Represents the votes and score for a specific council.
-     */
-    struct CouncilVotes {
+    struct CouncilVotes{
         bytes32 councilId;
-        VotingParameters votingParameters;
+        MembershipManagement.VotingParameters votingParameters;
         CouncilGroupVotes[] votes;
         int score;
     }
-
-    /**
-     * @dev Represents the votes and score for a council group.
-     */
-    struct CouncilGroupVotes {
+    struct CouncilGroupVotes{
         uint groupId;
         Vote[] votes;
         int score;
     }
-
-    /**
-     * @dev Enumerates the different types of proposals.
-     */
     enum ProposalTypes {
         Membership,
         MinVoteDuration,
         UpdateVotingParameters
     }
-
-    /**
-     * @dev Enumerates the different statuses of a proposal.
-     */
     enum ApprovalStatus {
         Entered,
         Pending,
@@ -134,11 +88,7 @@ library MembershipManagement {
         Approved,
         Rejected
     }
-
-    /**
-     * @dev Represents the result of vote tallying for a proposal.
-     */
-    struct TallyResult {
+    struct TallyResult{
         CouncilVotes[] acceptedVotes;
         int score;
         ApprovalStatus status;
